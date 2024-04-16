eb6166ff6d91579ef3cde8879d95e3c68a1b5918
Bernardodass <bernardo113322@gmail.com>
2024-04-16 16:46:39 -0300

    Update 02-arrays.js

---
+++ js/02-arrays.js
@@ -0,0 +1,12 @@
+'use strict'
+
+// declarando um array de alunos
+const alunos = ["Wilian", "Vinicius", "Marli", "Duda", "Gabriel"];
+
+// Exibindo a estrutura do array (somente no console)
+console.log(alunos); 
+
+// acessando individualmente alguns alunos ATRAVÉS DO INDICE
+console.log(alunos[1]); // Vinicius
+console.log(alunos[3]); // Duda
+console.log(alunos[0]); // Wilian
