58d6e99d7a9786ebc6211b316ea75a676084f053
Bernardodass <bernardo113322@gmail.com>
2024-04-16 16:59:25 -0300

    Update 02-arrays.js

--- js/02-arrays.js
+++ js/02-arrays.js
@@ -10,3 +10,13 @@ console.log(alunos);
 console.log(alunos[1]); // Vinicius
 console.log(alunos[3]); // Duda
 console.log(alunos[0]); // Wilian
+
+/* Mini-exercício (FAÇA AQUI MESMO!)
+
+-Crie um novo array com o nome de 7 coisas que você gosta (artista, banda, música, livro...)
+
+-Em seguida, mostre uma frase personalizada no console indicando o nome do segundo, do quinto e do sétimo elemento do array. */
+
+const gostos = ["brigadeiro", "coxinha", "bolinha de queijo", "pao de queijo", "pao com manteiga", "croissaint", "pizza"];
+
+console.log(`Eu gosto de ${gostos[1]}, ${gostos[4]} e ${gostos[6]}.`);
