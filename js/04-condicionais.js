8ad6fc3bda03458feca2ace14d00b3109e052e8d
Bernardodass <bernardo113322@gmail.com>
2024-04-17 16:43:29 -0300

    Update 04-condicionais.js

--- js/04-condicionais.js
+++ js/04-condicionais.js
@@ -1,1 +1,32 @@
 'use strict';
+
+/* comandos utilizados em condicionais
+if (se)
+else (senão)
+else if (senão se)
+switch/case (escolha/caso) 
+
+operadores relacionais: 
+> maior que
+< menor que
+>= menor ou igual
+>= maior ou igual
+!= diferença
+== igualdade                                  */
+
+// condicional simples
+let numero = 6;
+
+if(numero > 5){
+    console.log("mensagem exibida pois a condição é VERDADEIRA");
+}
+
+// condicional composta
+let usuario = "Tukika Navara";
+let idade = 23;
+
+if(idade>=18){
+    console.log(`${usuario} é maior de idade.`);
+} else {
+console.log(`${usuario} é menor de idade.`);
+}
