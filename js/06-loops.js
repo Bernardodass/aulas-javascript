35885c7501ef8cc2af3005a4bc891417c5db2dd5
Bernardodass <bernardo113322@gmail.com>
2024-04-18 16:55:36 -0300

    Update 06-loops.js

--- js/06-loops.js
+++ js/06-loops.js
@@ -1,2 +1,21 @@
 'use strict'
 
+/* loops masi usados:
+- while (enquanto)
+- for (para) 
+
+normalmente, os loops são controlados através de uma variável contadora ou de alguma condição lógica para que o loop seja interrompido. */ 
+
+// loop WHILE
+/* let contador = 1;
+while(contador<=5){
+    console.log("valor do contador: "+contador);
+    contador++;
+} */
+
+// loop FOR
+for(let i = 1; i<=10; i++){
+    console.log("i vale: "+i);
+}
+
+/* obs: normalmente variaveis de controle de repetição sao chamadas de "i" "j" "k". */
