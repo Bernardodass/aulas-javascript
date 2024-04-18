27bd531ac51100e5a88185044000583aff15fd6c
Bernardodass <bernardo113322@gmail.com>
2024-04-18 15:29:19 -0300

    Update 05-operadores-logicos.js

--- js/05-operadores-logicos.js
+++ js/05-operadores-logicos.js
@@ -11,10 +11,30 @@ let faltas = 10;
 let resultado;
 
 // operador E (AND &&)
-if(mediafinal>=mediaminima && faltas<=limitedefaltas){
-    resultado = "aprovado";         
+if (mediafinal >= mediaminima && faltas <= limitedefaltas) {
+    resultado = "aprovado";
 } else {
     resultado = "reprovado";
 }
 console.log(resultado);
 console.log(`${mediafinal} ${resultado} foi bem`);
+
+console.log(`------------------`);
+
+let idade = 14;
+
+// operador OU/OR (||)     obs: o símbolo "|" é chamado de PIPE
+if (idade >= 18 || idade < 12) {
+    console.log("você é um adulto ou criança");
+} else {
+    console.log("você é um adolescente");
+}
+
+console.log("------------------");
+
+let onibus = 2203; // 2203 ou 2019
+if (onibus == 2203 || onibus == 2019) {
+    console.log("passa no senac, embarque!");
+} else {
+    console.log("não passa no senac, aguarde!");
+}
