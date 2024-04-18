079e5e8571d02bd7a9e18f08f044a808fff3fb0e
Bernardodass <bernardo113322@gmail.com>
2024-04-18 17:23:39 -0300

    Update 06-loops.js

--- js/06-loops.js
+++ js/06-loops.js
@@ -19,3 +19,30 @@ for(let i = 1; i<=10; i++){
 }
 
 /* obs: normalmente variaveis de controle de repetição sao chamadas de "i" "j" "k". */
+
+
+// loop para acessar arrays
+const alunos = ["chaves", "chapolin", "seu madruga", "tukika navara", "socabe rola"];
+
+/* for(let i = 0; i< alunos.length; i++){
+    console.log(alunos[i]);
+} */
+
+// loop exclusivo do JS para arrays: for/of
+for(const aluno of alunos){
+    console.log(aluno);
+}
+
+// loop exclusivo do JS para objetos: for/in
+const livro = {
+    titulo: "HTML5",
+    autor: "Maujor",
+    site: "http://maujor.com",
+    ano: 2016
+};
+
+console.log("--------------------");
+
+for(const prop in livro){
+    console.log(livro[prop]);
+}
