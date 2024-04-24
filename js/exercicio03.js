31612e979f407956c02edde430cebdf016f8e1b7
Bernardodass <bernardo113322@gmail.com>
2024-04-24 16:08:10 -0300

    Update exercicio03.js

--- js/exercicio03.js
+++ js/exercicio03.js
@@ -1,20 +1,20 @@
 'use strict';
 
-let cotacaoDoDia = 5.15;
+let cotacaoDoDia = 5.16;
 let valorEmDolar = 1000;
 
 function converterEmReais(valor){
     return valor * cotacaoDoDia;
 }
 
-function formatarMoeda(valor){
+function formatarMoeda(valor, siglaDaMoeda, localizacao){
     const opcoes = {
         style: "currency",
-        currency: "BRL"
+        currency: siglaDaMoeda
     };
 
     return new Intl.NumberFormat(
-        "pt-br", opcoes
+        localizacao, opcoes
     ).format(valor);
 }
 
@@ -23,9 +23,12 @@ console.log( converterEmReais(valorEmDolar) );
 
 // Versão 1 (função como parâmetro para outra função) com formatação
 console.log( 
-    formatarMoeda( converterEmReais(valorEmDolar) ) 
+    formatarMoeda( converterEmReais(valorEmDolar), "BRL", "pt-br" ) 
 );
 
 // Versão 2 (primeiro, guardamos o resultado da conversão; depois passamos o resultado para a formatação)
 let resultadoEmReais = converterEmReais(valorEmDolar);
-console.log( formatarMoeda(resultadoEmReais) );
+console.log( formatarMoeda(resultadoEmReais, "BRL", "pt-br") );
+
+// Formatando o valor em dólar
+console.log( formatarMoeda(valorEmDolar, "USD", "en") );
